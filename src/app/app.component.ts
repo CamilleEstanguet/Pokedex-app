b082f7d591ff02ffaa9b20149da6ecd0a89af255
camilleEstanguet-Neocase <cestanguet@neocasesoftware.com>
2024-01-11 22:24:52 +0100

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core"
+import { CommonModule } from "@angular/common"
+import { RouterOutlet } from "@angular/router"
+import { POKEMONS } from "../pokemons/mock-pokemon"
+import { Pokemon } from "../pokemons/pokemon"
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
+  templateUrl: "app.component.html",
   styles: [],
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS
+  pokemonSelected: Pokemon | undefined
+  justLoaded: boolean = true
+
+  ngOnInit(): void {
+    console.table(this.pokemonList)
+  }
+
+  selectPokemon(pokemonId: string) {
+    const id = +pokemonId
+    this.pokemonSelected = this.pokemonList.find((pokemon) => pokemon.id == +pokemonId)
+    if(this.justLoaded == true) 
+      this.justLoaded = false
+  }
+
+  searchPokemon(pokemonName: string) {
+    const name = pokemonName
+    this.pokemonSelected = this.pokemonList.find((pokemon) => pokemon.name == pokemonName)
+    if(this.justLoaded == true) 
+      this.justLoaded = false
+  }
 }
